e33c981c2cf0fa6321da88f6f2ef4f274da4e68e
Daniel Santos <dados.dan.santos@gmail.com>
2018-07-13 02:58:21 -0300

    Adds a board model to the app

--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
 const title = "Sodoku App";
 window.document.title = title;
+
+//Square prototype
+const square = `
+    <div class="square"></div>
+`;
+
+//Mini square prototype
+const miniSqr = `
+    <div class="miniSqr" onClick="nmbrpicker(this)">
+        <input type="text">
+    </div>
+`;
+const board = document.getElementById('mainBrd');
+
+
+//Adds the Squares
+for (let i = 0; i < 9; i++) {
+    board.innerHTML += square;
+}
+
+const sqrClass = document.getElementsByClassName('square');
+
+
+
+//Adds the mini squares
+for (let i = 0; i < sqrClass.length; i++) {
+    for (let j = 0; j < sqrClass.length; j++) {
+        sqrClass[i].innerHTML += miniSqr;
+    }
+}
+
+
+
